9c5621425123068f1fa7306e266533d90e49ccf5
Lizeth Lucero <lizethlucero@yahoo.com>
2019-11-25 01:20:37 -0500

    Connected sensors with motors

--- proxsens/proxsens.ino
+++ proxsens/proxsens.ino
@@ -1,6 +1,11 @@
+#include <Arduino.h>
+#include <stdint.h>
+#include <math.h>
+
 #define TRIG 0
 #define ECHO 1
-#define LED 2
+#define SECTRIG 6
+#define SECECHO 12
 #define OVERSMPL 5
  
 float waveTime, distance, curr_dist;
@@ -9,32 +14,49 @@ void setup();
 void loop();
 float readSensor();
 float sampling();
+void dist_motor1(float power);
+void vel_motor1(float power);
+
+float readSensor2();
+float sampling2();
+void dist_motor2(float power);
 
 void setup() {
   Serial.begin (9600);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
-  pinMode(LED, OUTPUT); //currently testing with LED, will replace with haptic motor
-  pinMode(LED,LOW); //currently testing with LED, will replace with haptic motor
+  pinMode(SECTRIG, OUTPUT);
+  pinMode(SECECHO, INPUT);
+  //pinMode(LED, OUTPUT); //currently testing with LED, will replace with haptic motor
+  //pinMode(LED,LOW); //currently testing with LED, will replace with haptic motor
+  
+  // Set bit 0 in port D to output
+  DDRD |= (1 << PORTD0);
+  // Clear PORTD0 in setup
+  PORTD &= ~(1 << PORTD0);
+
+  // Set bit 0 in port D to output
+  DDRB |= (1 << PORTB6);
+  // Clear PORTD0 in setup
+  PORTB &= ~(1 << PORTB6);
+
 }
  
 void loop() { 
-   
 
   //calculating the average distance to display from 5 samplings
+  //float avg_dist = 250.15;
+  
   float avg_dist = sampling();
   Serial.print(avg_dist);
-  Serial.println(" cm");
-  //we think that if an object is a metre away,the user should be aware
-  //through the haptic motor. We are currently testing with LED
-  if(avg_dist < 100.0){
-    digitalWrite(LED,HIGH);
-  }
-  else{
-    digitalWrite(LED,LOW);
-  }
-  delay(10);
-  
+  Serial.println(" cm for motor 1");
+  dist_motor1(avg_dist);
+
+  float avg_dist_m2 = sampling2();
+  Serial.print(avg_dist_m2);
+  Serial.println(" cm for motor 2");
+  dist_motor2(avg_dist_m2);
+
 }
 
 /*
@@ -50,16 +72,35 @@ void loop() {
 float readSensor(){
   // Write to the Trigger Pin to begin sending wave
   digitalWrite(TRIG, LOW);
+  //digitalWrite(TRIG2,LOW)
   delayMicroseconds(2);
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
-  delay(60); // A delay of 60ms to allow wave to travel
+  delay(2); // A delay of 60ms to allow wave to travel
 
   // Reading the Echo Pin
   waveTime = pulseIn(ECHO, HIGH);
   // Speed of sound is 343 m/s, use to determine distance from our waveTime
-  distance = (waveTime / 2) * 0.0343;
+  distance = (waveTime / 2.0) * 0.0343;
+  return distance;
+}
+
+float readSensor2(){
+  // Write to the Trigger Pin to begin sending wave
+  digitalWrite(SECTRIG, LOW);
+  //digitalWrite(TRIG2,LOW)
+  delayMicroseconds(2);
+  digitalWrite(SECTRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(SECTRIG, LOW);
+  delay(2); // A delay of 60ms to allow wave to travel
+
+  // Reading the Echo Pin
+  waveTime = pulseIn(SECECHO, HIGH);
+  // Speed of sound is 343 m/s, use to determine distance from our waveTime
+  distance = (waveTime / 2.0) * 0.0343;
+  return distance;
 }
 
 //This function calculates the average of five reads and returns it
@@ -67,10 +108,62 @@ float sampling(){
   float reads = 0;
   for (int i = 0; i < OVERSMPL; i++){
     curr_dist = readSensor(); //calls the read function once
+
     //if our distance is in range add to our reads in order to average
-    if (curr_dist <= 400 && curr_dist >= 2) {
+    if (curr_dist <= 400.0 && curr_dist >= 2.0) {
       reads += curr_dist;
     }
   }
-  return reads;
+  return reads/OVERSMPL;
+}
+
+//This function calculates the average of five reads and returns it
+float sampling2(){
+  float reads = 0;
+  for (int i = 0; i < OVERSMPL; i++){
+    curr_dist = readSensor2(); //calls the read function once
+
+    //if our distance is in range add to our reads in order to average
+    if (curr_dist <= 400.0 && curr_dist >= 2.0) {
+      reads += curr_dist;
+    }
+  }
+  return reads/OVERSMPL;
+}
+
+void dist_motor1(float power){
+  float avg_dis = power*100;
+  
+  // Set PORTD0 for 1 ms
+  PORTD |= (1 << PORTD0);
+  delayMicroseconds(40000.0-avg_dis);
+  // Clear PORTD0
+  PORTD &= ~(1 << PORTD0);
+  delayMicroseconds(avg_dis);
+  
+}
+
+void dist_motor2(float power){
+  float avg_dis = power*100;
+  
+  // Set PORTD0 for 1 ms
+  PORTB |= (1 << PORTB6);
+  delayMicroseconds(40000.0-avg_dis);
+  // Clear PORTD0
+  PORTB &= ~(1 << PORTB6);
+  delayMicroseconds(avg_dis);
+  
+}
+
+
+void vel_motor1(float power){
+  float avg_dist = power*10;
+
+  
+  // Set PORTD0 for 1 ms
+  PORTD |= (1 << PORTD0);
+  delayMicroseconds(avg_dist);
+  // Clear PORTD0
+  PORTD &= ~(1 << PORTD0);
+  delayMicroseconds(4000.0-avg_dist);
 }
